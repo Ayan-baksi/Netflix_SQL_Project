7003d7256e11579eda03221291c77bf4250c6702
Ayan Baksi <ayanbaksi11@gmail.com>
2025-09-25 19:22:38 +0530

    Update Business Queries Netflix.sql

--- Business Queries Netflix.sql
+++ Business Queries Netflix.sql
@@ -1,37 +1,47 @@
-All differnt queries
+All differnt queries-
+
+1. How many movies and TV shows are available on Netflix?
+
+2.Find the most common rating (like TV-MA, TV-14) on Netflix.
+
+3.List the top 10 countries with the most content on Netflix.
+
+4. How many movies were released each year on Netflix?
+
+5. Which year had the highest number of releases?
+
+6. Find the top 5 directors with the most movies/TV shows.
+
+7. What are the top 10 most common genres (listed in the listed_in column)?
+
+8. Count how many titles were added each year.
+
+9. Find the average duration of Movies.
+
+10. Show the distribution of TV Shows by number of seasons.
+
+11. Which country has produced the most TV Shows?
+
+12. Which actor/actress appeared most frequently on Netflix titles?
+
+13. Find the titles that were released in India.
+
+14. List all movies released after 2015.
+
+15. Count how many Movies vs TV Shows were added in the last 5 years.
+
+16. Find titles that have more than 1 genre assigned.
+
+17. Get the percentage of Movies vs TV Shows on Netflix.
+
+18. Find the oldest movie available on Netflix (by release year).
+
+19. Show the number of content added in each month (seasonality).
+
+20. Find how many titles do not have a director assigned.
+
+21. List the top 5 longest movies (by duration).
+
+22. Using a CTE, show the cumulative number of titles released year by year.
 
 
-1. Count the number of Movies vs TV Shows
-  
-2. Find the most common rating for movies and TV shows
-  
-3. List all movies released in a specific year (e.g., 2020)
-  
-4. Find the top 5 countries with the most content on Netflix
-  
-5. Identify the longest movie
-  
-6. Find content added in the last 5 years
-  
-7. Find all the movies/TV shows by director 'Rajiv Chilaka'!
-
-  
-8. List all TV shows with more than 5 seasons
-  
-9. Count the number of content items in each genre
-
-  
-10.Find each year and the average numbers of content release in India on netflix. 
-return top 5 year with highest avg content release!
-  
-11. List all movies that are documentaries
-  
-12. Find all content without a director
-  
-13. Find how many movies actor 'Salman Khan' appeared in last 10 years!
-  
-14. Find the top 10 actors who have appeared in the highest number of movies produced in India.
-  
-15.Categorize the content based on the presence of the keywords 'kill' and 'violence' in 
-the description field. Label content containing these keywords as 'Bad' and all other 
-content as 'Good'. Count how many items fall into each category.
